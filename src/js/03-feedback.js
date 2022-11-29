041c5994c3fc6972539260367c244c585e3e5238
AlinaBanias <isabella08092001@gmail.com>
2022-11-29 18:25:11 +0200

    Task 2 and 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+ const STORAGE_KEY = 'feedback-form-state';
+
+ const feedbackFormData = {};
+
+refs.form.addEventListener('submit' , onFormSubmit);
+refs.textarea.addEventListener('input' , throttle(onTextareaInput , 500));
+
+refs.form.addEventListener('input' , e => {
+
+    feedbackFormData[e.target.name] = e.target.value;
+
+    console.log(formData);
+})
+populateTextarea ();
+
+function onFormSubmit(evt) {
+evt.preventDefault();
+
+evt.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+    const message =evt.target.value;
+
+    localStorage.setItem(STORAGE_KEY , message);
+}
+
+function populateTextarea () {
+const savedMessage  = localStorage.getItem(STORAGE_KEY);
+if(savedMessage) {
+    console.log(savedMessage);
+    refs.textarea.value = savedMessage;
+}
+
+}
