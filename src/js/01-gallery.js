8b20410fd4f7fe883512aa9ab70dc86355bcb7ec
AlinaBanias <isabella08092001@gmail.com>
2022-11-29 15:10:32 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+console.log(SimpleLightbox);
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createImageMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+ 
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+   
+   const isGalleryItem = event.target.classList.contains("gallery__image");
+   if(!isGalleryItem) {
+       return;
+   }
+   console.log(isGalleryItem);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+captionsData: "alt",
+captionDelay: 250,
+});
+
+function createImageMarkup(images) {
+    return images.map(({ preview , original , description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}"
+        alt="${description}" />
+    </a >
+        `;
+    }).
+    join("");
+  
+}
