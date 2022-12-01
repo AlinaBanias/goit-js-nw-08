c32a8f557ebc787b3abf09b31cc5d5de4d99edf1
AlinaBanias <isabella08092001@gmail.com>
2022-12-01 18:06:18 +0200

    Work with mistakes 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,17 +9,13 @@ const refs = {
 const feedbackFormData = {};
 
 refs.form.addEventListener('submit' , onFormSubmit);
-refs.textarea.addEventListener('input' , throttle(onTextareaInput , 500));
-
-refs.form.addEventListener('input' , e => {
+refs.form.addEventListener('input' , throttle((e => {
 
  feedbackFormData[e.target.name] = e.target.value;
-localStorage.setItem( STORAGE_KEY, JSON.stringify(feedbackFormData));
-const savedData = localStorage.getItem(STORAGE_KEY, feedbackFormData);
-const parsedData = JSON.parse(savedData);
-console.log(parsedData);
-console.log(feedbackFormData);
-   });
+ localStorage.setItem( STORAGE_KEY, JSON.stringify(feedbackFormData));
+
+ } ), 500));
+ 
 
 populateTextarea ();
 
@@ -30,17 +26,12 @@ evt.currentTarget.reset();
 localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextareaInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem(STORAGE_KEY , message);
- }
 
 function populateTextarea () {
-const savedMessage  = localStorage.getItem(STORAGE_KEY);
-if(savedMessage) {
-    console.log(savedMessage);
-    // savedMessage[e.target.name] = e.target.value;
-    
+const savedData  = localStorage.getItem(STORAGE_KEY);
+const parsedData = JSON.parse(savedData);
+if(parsedData) {
+    console.log(parsedData);  
 }
 
 }
